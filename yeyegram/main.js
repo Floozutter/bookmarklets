a80befcce1c2329490523ab7684da976db9b20c2
Floozutter <floozutter@gmail.com>
2020-08-04 22:13:37 -0700

    Decode solution from the page's global variable d

--- yeyegram/main.js
+++ yeyegram/main.js
@@ -1,2 +1,23 @@
 (() => {
+	const rowCount = (d[2][0] % d[2][3]) + (d[2][1] % d[2][3]) - (d[2][2] % d[2][3]);  // C
+	const colCount = (d[1][0] % d[1][3]) + (d[1][1] % d[1][3]) - (d[1][2] % d[1][3]);  // D
+	const Aa = (d[3][0] % d[3][3]) + (d[3][1] % d[3][3]) - (d[3][2] % d[3][3]);
+	const V = Aa + 5;
+	const Ha = (d[V][0] % d[V][3]) * (d[V][0] % d[V][3]) + (d[V][1] % d[V][3]) * 2 + (d[V][2] % d[V][3]);
+	const Ia = d[V + 1];
+	
+	const solution = [];  // E
+	// Initialize solution as an Array of Arrays of zeros.
+	for (let rowIdx = 0; rowIdx < rowCount; rowIdx++) {
+		solution[rowIdx] = Array(colCount).fill(0);
+	}
+	// Populate solution.
+	for (let x = V + 2; x <= V + 1 + Ha; x++) {
+		for (let v = d[x][0] - Ia[0] - 1; v < d[x][0] - Ia[0] + d[x][1] - Ia[1] - 1; v++) {
+			solution[d[x][3] - Ia[3] - 1][v] = d[x][2] - Ia[2];
+		}
+	}
+	
+	let rowRuns = [];  // N
+	let colRuns = [];  // O
 })();
